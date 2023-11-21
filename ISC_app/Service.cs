5e86e3019efeece26f539c9df0eed8abea927acc
Petrik Diak <diak@petrik.hu>
2023-11-21 11:24:17 +0100

    Add project files.

---
+++ ISC_app/Service.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISC_app
+{
+    class Service
+    {
+        private HttpClient client = new HttpClient();
+        private string url = "https://retoolapi.dev/oahQtd/data";
+
+        public List<User> GetAll()
+        {
+            string json = client.GetStringAsync(url).Result;
+            return JsonConvert.DeserializeObject<List<User>>(json);
+        }
+	}
+}
---
+++ ISC_app/User.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISC_app
+{
+    class User
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+		[JsonProperty("CVV")]
+		public string CVV { get; set; }
+
+		[JsonProperty("Name")]
+		public string Name { get; set; }
+
+		[JsonProperty("Balance")]
+		public int Balance { get; set; }
+
+		[JsonProperty("Credit_card")]
+		public string Credit_card { get; set; }
+    }
+}
